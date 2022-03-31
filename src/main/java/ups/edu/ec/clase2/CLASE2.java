1aafdb7c0bb2af05f4f5a4a17180ec6430fd9aec
PC-08 <PC-08@DESKTOP-GRDGBOO>
2022-03-31 07:19:32 -0500

    final

---
+++ src/main/java/ups/edu/ec/clase2/CLASE2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ups.edu.ec.clase2;
+
+/**
+ *
+ * @author PC-08
+ */
+public class CLASE2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
